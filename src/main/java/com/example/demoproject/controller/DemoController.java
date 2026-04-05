5a396a1614f8dc06ba10a33210dce3b801864cde
vladimir.beglyak <vladimir.beglyak@solva.kz>
2026-04-05 20:23:46 +0300

    feat(my-app): add logs

--- src/main/java/com/example/demoproject/controller/DemoController.java
+++ src/main/java/com/example/demoproject/controller/DemoController.java
@@ -4,6 +4,7 @@ import com.example.demoproject.model.UserResponse;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.logging.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/users")
 public class DemoController {
 
+  private static final Logger LOGGER = Logger.getLogger(DemoController.class.getName());
+
   @GetMapping
   public ResponseEntity<List<UserResponse>> getUsers() {
+    LOGGER.info("getUsers called");
     return ResponseEntity.of(Optional.of(List.of(new UserResponse(UUID.randomUUID(), "test2@gamil.com"))));
   }
 }
